43eb2f2d5b413047108a20401b571468ae8d58ad
joelt006 <8281o29196@gmail.com>
2025-04-15 14:04:35 +0200

    history

--- .history/mybank/mybank/settings_20250327124155.py
+++ .history/mybank/mybank/settings_20250327124155.py
@@ -76,12 +76,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mybank.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # DATABASES = {
